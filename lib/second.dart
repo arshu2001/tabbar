02f90a375d842b70f5ae8589e5c09a66374b64fd
arshu2001 <128467852+arshu2001@users.noreply.github.com>
2024-02-15 20:18:59 +0530

    third commit

--- lib/second.dart
+++ lib/second.dart
@@ -8,12 +8,16 @@ class Second extends StatefulWidget {
 }
 
 class _SecondState extends State<Second> {
+  List name = ['Arshad','Favas','Fuaad','shakir','shibin'];
+  List sub =  ['enthalla', 'how are you', 'daa', 'enthh', 'oook'];
+  List date = ['8:13','6:00','5:45','12:13','1.15.24'];
+  List msg = ['4','8','45','8','54'];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
        body: Expanded(
          child: ListView.separated(
-          itemCount: 6,
+          itemCount: name.length,
           separatorBuilder: (context, index) {
             return Padding(
               padding: const EdgeInsets.only(left: 20,right: 20),
@@ -22,17 +26,20 @@ class _SecondState extends State<Second> {
           },
           itemBuilder: (context, index) {
             return ListTile(
-              title: Text('Black'),
-              subtitle: Text('oversized tshirt size XS'),
+              title: Text(
+                name[index],
+                style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              subtitle: Text(sub[index]),
               leading:  CircleAvatar(backgroundImage: NetworkImage('https://images.pexels.com/photos/236047/pexels-photo-236047.jpeg?cs=srgb&dl=clouds-cloudy-countryside-236047.jpg&fm=jpg'),),
               trailing: Column(
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
-                  Text('17.01.24'),
+                  Text(date[index]),
                   Container(
                     child: Padding(
                       padding: const EdgeInsets.only(left: 4,right: 4),
-                      child: Text('5'),
+                      child: Text(msg[index]),
                     ),
                      decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),color: Colors.green),
                   )
