2e8db2e1cbe2104482ec805c880afb3a86c3af23
arshu2001 <128467852+arshu2001@users.noreply.github.com>
2024-02-15 19:22:49 +0530

    second commit

--- lib/second.dart
+++ lib/second.dart
@@ -11,7 +11,40 @@ class _SecondState extends State<Second> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       body: Center(child: Text('Second page',style: TextStyle(fontSize: 50),)),
+       body: Expanded(
+         child: ListView.separated(
+          itemCount: 6,
+          separatorBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.only(left: 20,right: 20),
+              child: Divider(),
+            );
+          },
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text('Black'),
+              subtitle: Text('oversized tshirt size XS'),
+              leading:  CircleAvatar(backgroundImage: NetworkImage('https://images.pexels.com/photos/236047/pexels-photo-236047.jpeg?cs=srgb&dl=clouds-cloudy-countryside-236047.jpg&fm=jpg'),),
+              trailing: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text('17.01.24'),
+                  Container(
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 4,right: 4),
+                      child: Text('5'),
+                    ),
+                     decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),color: Colors.green),
+                  )
+
+                ],
+              ),
+         
+            );
+          },
+         
+         ),
+       ),
     );
   }
 }
